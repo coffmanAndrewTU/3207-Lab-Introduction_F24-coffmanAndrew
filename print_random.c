e1e32d18d4b652fe75a8b9243113c320c19f6267
andrewCoffman <tuq12108@temple.edu>
2024-08-29 19:15:05 -0400

    added rand_string and main to print_random.c

--- print_random.c
+++ print_random.c
@@ -1,4 +1,20 @@
 //Given Project code
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+#include "random.h"
+
+int rand_string(char *s, size_t size);
+char* rand_string_alloc(size_t size);
+
+int main(){
+    // 7 chosen for size because that is what is specified in the homework pdf
+    char *randomString = rand_string_alloc(7);
+    printf("%s");
+    free(*randomString);
+    randomString = NULL;
+}
+
 char* rand_string_alloc(size_t size)
 {
      char *s = malloc(size + 1);
@@ -6,4 +22,13 @@ char* rand_string_alloc(size_t size)
          rand_string(s, size);
      }
      return s;
+}
+
+int rand_string(char *s, size_t size){
+    char *t = s;
+    for(int i = 0; i < size; ++i){
+        *t = randchar();
+        ++t;
+    }
+    *t = '\0';
 }
